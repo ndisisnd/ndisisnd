7abc7ed68ab49b5b292a8722eeb8e9682881658f
Andy Chan <ndisisnd@gmail.com>
2022-03-04 00:35:29 +0800

    Fixing favicon v0.1 build

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
           rel="stylesheet"
         />
+        <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <body>
         <Main />
