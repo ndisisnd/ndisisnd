0e1461a081b79a0e8a1a3187ede0627b80264938
Andy Chan <ndisisnd@gmail.com>
2022-05-17 20:55:58 +0800

    feat(Coming Soon page): :sparkles:  Add new coming soon page

---
+++ src/pages/coming-soon.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Layout from "../components/layout"
+
+function ComingSoon() {
+  return (
+    <Layout>
+      <h1 className="text-5xl font-bold py-12">I'm working on it!</h1>
+      <section className="flex flex-col">
+        <p>It's currently under construction, so do hang on for a bit.</p>
+        <p>
+          Need to reach out to me? You can always contact me at
+          ndisisnd[at]gmail[dot]com.
+        </p>
+      </section>
+    </Layout>
+  )
+}
+
+export default ComingSoon
